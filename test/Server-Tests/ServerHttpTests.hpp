3d0b0552f31442f0ca8cd4e65169461559ff225a
CorrupD <nijenkamp.b@gmail.com>
2020-04-15 16:11:31 +0200

    Improved tests setup.

---
+++ test/Server-Tests/ServerHttpTests.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <pch.hpp>
+#include <gtest/gtest.h>
+
+TEST(HALLO, HALLO)
+{
+    ASSERT_TRUE(true);
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <Server-Tests/ServerHttpTests.hpp>
 
 TEST(ExampleTests, DemonstrateGTestMacros)
 {
