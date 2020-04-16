cc7e82e9b30114084fe95fea8c3dbde5fe6c71d6
Veliks <felixbeuving@live.nl>
2020-04-16 11:29:50 +0200

    EngineTests

---
+++ test/EngineTests/BufferTests.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <pch.hpp>
+#include <gtest/gtest.h>
+
+TEST(BufferTests, DTests)
+{
+    EXPECT_TRUE(true);
+}
+
--- test/ExampleTests.cpp
+++ test/ExampleTests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <EngineTests/BufferTests.hpp>
 
 TEST(ExampleTests, DemonstrateGTestMacros)
 {
