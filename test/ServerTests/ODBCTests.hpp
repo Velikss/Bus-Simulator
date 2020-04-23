d4be33eafb0ca5a2204b7f48dc5c890c7af4c153
Bram Nijenkamp <bram.nijenkamp@dataaccess.eu>
2020-04-23 11:00:14 +0200

    shared connect string

--- test/ServerTests/ODBCTests.hpp
+++ test/ServerTests/ODBCTests.hpp
@@ -6,7 +6,7 @@ cODBCInstance oInstance;
 
 TEST(ODBCTests, Connect)
 {
-    EXPECT_TRUE(oInstance.Connect("driver={MariaDB ODBC 3.1 Driver};server=192.168.178.187;user=root;pwd=hiddenhand;database=test;"));
+    EXPECT_TRUE(oInstance.Connect("driver=MariaDB ODBC 3.1 Driver;server=192.168.178.187;user=root;pwd=hiddenhand;database=test;"));
 }
 
 TEST(ODBCTests, DisConnect)
