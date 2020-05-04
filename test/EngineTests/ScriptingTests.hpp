3a0636640350f1c8ab7f6ff98c582a587acb0c3f
Felix <felixbeuving@live.nl>
2020-05-04 15:08:57 +0200

    Attempt to fix unit tests

--- test/EngineTests/ScriptingTests.hpp
+++ test/EngineTests/ScriptingTests.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include <pch.hpp>
 #include <gtest/gtest.h>
+#include <engine/src/scripting/cScriptingEngine.hpp>
+#include <engine/src/entities/cEntity.hpp>
 
 TEST(ScriptingTests, InitEngine)
 {
+    cEntity entity(nullptr);
 }
