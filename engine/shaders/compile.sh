8f89b10ffd0e4ea44ecb472fb1d7fdf1a70147fe
Koenn <koenn@bits.team>
2020-05-06 13:01:09 +0200

    shader compile script fix

--- engine/shaders/compile.sh
+++ engine/shaders/compile.sh
@@ -1,4 +1,11 @@
-glslc shader.vert -o vert.spv
-glslc shader.frag -o frag.spv
-glslc text.frag -o text.frag.spv
-glslc text.vert -o text.vert.spv
+if [[ -z "${VULKAN_SDK}" ]]; then
+    glslc shader.vert -o vert.spv
+    glslc shader.frag -o frag.spv
+    glslc text.frag -o text.frag.spv
+    glslc text.vert -o text.vert.spv
+else
+    "${VULKAN_SDK}/bin/glslc" shader.vert -o vert.spv
+    "${VULKAN_SDK}/bin/glslc" shader.frag -o frag.spv
+    "${VULKAN_SDK}/bin/glslc" text.frag -o text.frag.spv
+    "${VULKAN_SDK}/bin/glslc" text.vert -o text.vert.spv
+fi
