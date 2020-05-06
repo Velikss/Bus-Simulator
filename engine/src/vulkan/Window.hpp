419d02cb260317d58cd50d498e3cad287bae6160
Koenn <koenn@bits.team>
2020-05-06 10:59:59 +0200

    Re-enable cursor and remove incorrect shader build command

--- engine/src/vulkan/Window.hpp
+++ engine/src/vulkan/Window.hpp
@@ -89,7 +89,7 @@ void cWindow::CreateGLWindow()
     ppWindow = glfwCreateWindow(WIDTH, HEIGHT, "BUS", nullptr, nullptr);
 
     glfwSetCursorPosCallback(ppWindow, mouseCallback);
-    glfwSetInputMode(ppWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //glfwSetInputMode(ppWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetKeyCallback(ppWindow, keyCallback);
 }
 
