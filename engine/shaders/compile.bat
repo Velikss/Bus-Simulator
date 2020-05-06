7a8d69560ca303e6294a7f40459626975577bb88
Koenn <koenn@bits.team>
2020-05-06 10:30:22 +0200

    Shader compile script fix

--- engine/shaders/compile.bat
+++ engine/shaders/compile.bat
@@ -2,6 +2,4 @@
 %VULKAN-SDK%/Bin32/glslc.exe shader.frag -o frag.spv
 %VULKAN-SDK%/Bin32/glslc.exe text.vert -o text.vert.spv
 %VULKAN-SDK%/Bin32/glslc.exe text.frag -o text.frag.spv
-D:/VulkanSDK/1.2.135.0/Bin32/glslc.exe text.frag -o text.frag.spv
-D:/VulkanSDK/1.2.135.0/Bin32/glslc.exe text.vert -o text.vert.spv
 pause
