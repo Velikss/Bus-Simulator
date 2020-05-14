3b0ce48840a1b88fc7cff7f5cbf3982d8915fbfc
Felix <felixbeuving@live.nl>
2020-05-14 10:45:56 +0200

    Proper braking?

--- engine/src/vulkan/entities/cBus.hpp
+++ engine/src/vulkan/entities/cBus.hpp
@@ -13,13 +13,14 @@ public:
 
     cBus(cMesh * mesh) : cBaseObject(mesh)
     {
-        pfMaxSpeed = 35;
-        pfMinSpeed = -12;
-        pfCurrentSpeed = 1;
+        pfMaxSpeed = 40;
+        pfMinSpeed = -15;
+        pfCurrentSpeed = 0;
         pfAcceleration = 0;
     }
 
     float CalculateAcceleration();
+    float CalculateBrakeForce();
     void Move();
     void Throttle();
     void Brake();
@@ -38,29 +39,45 @@ void cBus::Throttle() {
 
 void cBus::Brake() {
     if (pfCurrentSpeed > pfMinSpeed)
-        pfCurrentSpeed-= 0.2;
+        pfCurrentSpeed-= CalculateBrakeForce();
 }
 
 float cBus::CalculateAcceleration() {
     if((pfMaxSpeed * 0.1) >= pfCurrentSpeed)
-        return pfAcceleration = 0.1;
-    if((pfMaxSpeed * 0.2) >= pfCurrentSpeed)
-        return pfAcceleration = 0.2;
-    if((pfMaxSpeed * 0.3) >= pfCurrentSpeed)
-        return pfAcceleration = 0.3;
-    if((pfMaxSpeed * 0.4) >= pfCurrentSpeed)
-        return pfAcceleration = 0.4;
-    if((pfMaxSpeed * 0.5) >= pfCurrentSpeed)
-        return pfAcceleration = 0.5;
-    if((pfMaxSpeed * 0.6) >= pfCurrentSpeed)
-        return pfAcceleration = 0.6;
-    if((pfMaxSpeed * 0.7) >= pfCurrentSpeed)
-        return pfAcceleration = 0.7;
-    if((pfMaxSpeed * 0.8) >= pfCurrentSpeed)
-        return pfAcceleration = 0.8;
-    if((pfMaxSpeed * 0.9) >= pfCurrentSpeed)
-        return pfAcceleration = 0.9;
-    if((pfMaxSpeed * 1.0) >= pfCurrentSpeed)
         return pfAcceleration = 1.0;
+    if((pfMaxSpeed * 0.2) >= pfCurrentSpeed)
+        return pfAcceleration = 0.9;
+    if((pfMaxSpeed * 0.3) >= pfCurrentSpeed)
+        return pfAcceleration = 0.8;
+    if((pfMaxSpeed * 0.4) >= pfCurrentSpeed)
+        return pfAcceleration = 0.7;
+    if((pfMaxSpeed * 0.5) >= pfCurrentSpeed)
+        return pfAcceleration = 0.6;
+    if((pfMaxSpeed * 0.6) >= pfCurrentSpeed)
+        return pfAcceleration = 0.5;
+    if((pfMaxSpeed * 0.7) >= pfCurrentSpeed)
+        return pfAcceleration = 0.4;
+    if((pfMaxSpeed * 0.8) >= pfCurrentSpeed)
+        return pfAcceleration = 0.3;
+    if((pfMaxSpeed * 0.9) >= pfCurrentSpeed)
+        return pfAcceleration = 0.2;
+    if((pfMaxSpeed * 1.0) >= pfCurrentSpeed)
+        return pfAcceleration = 0.1;
 }
 
+float cBus::CalculateBrakeForce() {
+    if((pfMinSpeed * 0.7) <= pfCurrentSpeed)
+        return pfAcceleration = 0.4;
+    if((pfMinSpeed * 0.6) <= pfCurrentSpeed)
+        return pfAcceleration = 0.5;
+    if((pfMinSpeed * 0.5) <= pfCurrentSpeed)
+        return pfAcceleration = 0.6;
+    if((pfMinSpeed * 0.4) <= pfCurrentSpeed)
+        return pfAcceleration = 0.7;
+    if((pfMinSpeed * 0.3) <= pfCurrentSpeed)
+        return pfAcceleration = 0.8;
+    if((pfMinSpeed * 0.2) <= pfCurrentSpeed)
+        return pfAcceleration = 0.9;
+    if((pfMinSpeed * 0.1) <= pfCurrentSpeed)
+        return pfAcceleration = 1.0;
+}
