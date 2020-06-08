918f1375ba824407ba91a1813068a6ee53fb1f28
Bram Nijenkamp <bram.nijenkamp@dataaccess.eu>
2020-06-08 16:05:10 +0200

    In game impl
    Signed-off-by: Bram Nijenkamp <bram.nijenkamp@dataaccess.eu>

--- src/overlay/InGame.hpp
+++ src/overlay/InGame.hpp
@@ -1,6 +1,34 @@
 #pragma once
+#include <pch.hpp>
+#include <vulkan/module/overlay/OverlayWindow.hpp>
+#include <vulkan/texture/TextureHandler.hpp>
+#include <vulkan/util/GameManager.hpp>
 
-class InGame
+class cInGame : public cOverlayWindow
 {
+private:
+    iGameManager* ppOverlayProvider;
 
+public:
+    cInGame(iGameManager* pOverlayProvider) : ppOverlayProvider(pOverlayProvider)
+    {}
+
+protected:
+    void LoadTextures(cTextureHandler* pTextureHandler) override
+    {
+    }
+
+    void ConstructElements() override
+    {
+
+    }
+
+    void Tick() override
+    {
+    }
+
+    bool ShouldHandleInput() override
+    {
+        return true;
+    }
 };
